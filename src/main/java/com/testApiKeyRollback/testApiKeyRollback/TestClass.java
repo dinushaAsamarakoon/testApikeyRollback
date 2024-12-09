04bf82dfae4015be12d7e07195c515eb86df444d
DinushaSamarakoon <s.dinusha@gtngroup.com>
2024-12-09 17:33:53 +0530

    test class updated with dummy methods

--- src/main/java/com/testApiKeyRollback/testApiKeyRollback/TestClass.java
+++ src/main/java/com/testApiKeyRollback/testApiKeyRollback/TestClass.java
@@ -17,4 +17,28 @@ public class TestClass {
         String apiKey = "***REMOVED***";
         String password = "***REMOVED***";
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public String reverseString(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public String greetUser(String name) {
+        return "Hello, " + name + "!";
+    }
 }
